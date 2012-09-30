5eba50b635da403120136e02599a4f6dcd451956
aoner <aoners@gmail.com>
2012-09-30 17:16:55 +0800

    test

--- lib/wyqs.rb
+++ lib/wyqs.rb
@@ -1,26 +1,6 @@
+require 'wyqs/wyqs'
 module Wyqs
   class << self
     attr_accessor :appid, :app_secret, :site
-
-    def configure
-      yield self
-      true
-    end
   end
-
-  autoload :Client, "wyqs/client"
-=begin
-  autoload :Model, "taobao/model"
-  autoload :TaobaokeItem, "models/taobaoke_item"
-  autoload :TaobaokeShop, "models/taobaoke_shop"
-  autoload :TaobaokeReport, "models/taobaoke_report"
-  autoload :TaobaokeReportMember, "models/taobaoke_report_member"
-  autoload :Trade, "models/trade"
-  autoload :Order, "models/order"
-  autoload :PromotionDetail, "models/promotion_detail"
-  autoload :Item, "models/item"
-  autoload :Task, "models/task"
-  autoload :Subtask, "models/subtask"
-  autoload :Shop, "models/shop"
-=end
 end
