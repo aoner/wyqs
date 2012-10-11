e9ccf7c65f7cf93b02614b0e2e87fb482d49b828
aoner <aoners@gmail.com>
2012-10-11 21:40:06 +0800

    test

--- lib/wyqs/consumer.rb
+++ lib/wyqs/consumer.rb
@@ -1,35 +1,29 @@
 require 'json'
 module Wyqs
   class Consumer
-    attr_reader :appid, :app_secret, :site
-    attr_writer :options
+    attr_reader :appid, :app_secret,:params
+    attr_reader :accesstoken,:requesttoken,
 
-    def initialize(appid = Wyqs.appid, app_secret = Wyqs.app_secret, site = Wyqs.site)
+    def initialize(appid = Wyqs.appid, app_secret = Wyqs.app_secret)
       @appid = appid
       @app_secret = app_secret
       @site = site
-    end
-    
-    def get_request_token(options = {})
-      params = {
+      @params = {
         :timestamp => Time.now.to_i.to_s,
         :format => 'json',
         :appid => @appid,
         :authvers => '1.0',
         :signmethod => 'md5'
       }
-      params.merge!(options)
-      #str = (params.sort.collect { |c| "#{c[1]}" }).join("&")
-      #puts params
-      str = Digest::MD5.hexdigest((params.sort.collect { |c| "#{c[1]}" }).join("&")).downcase#"#{params[:appid]}&#{params[:authvers]}&#{params[:format]}&#{params[:signmethod]}&#{params[:timestamp]}"
-      #puts str
-      #sign = [app_secret,str,""].join("&")
-      #puts sign
-      params["sign"] = URI.encode(Base64.encode64s([app_secret,str,""].join("&")))
-      res = Net::HTTP.post_form(URI(@site), params)
-      #puts params
-      if params[:format] == 'json'
-        JSON.parse(res.body)
+    end
+    
+    def get_request_token(options = {})
+      @params.merge!(options)
+      str = Digest::MD5.hexdigest((@params.collect { |c| "#{c[1]}" }).join("&")).downcase
+      @params["sign"] = URI.encode(Base64.encode64s([app_secret,str,""].join("&")))
+      res = Net::HTTP.post_form(URI(@params[:site]), params)
+      if @params[:format] == 'json'
+      JSON.parse(res.body)
       else
         res.body
       end
